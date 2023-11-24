7dec6a0a5a4502a15441e910a11d4376606636dc
Hitesh Wadile <93307471+hitesh-wadile@users.noreply.github.com>
2023-11-24 07:45:25 +0530

    Time: 5 ms (13.31%) | Memory: 6.3 MB (73.70%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long ans = 0;
-        int flag = 0;
-        if(x<0){
-            x = abs(x);
-            flag = 1;
+        long r= 0;
+        while(x){
+            r = r*10 + x%10;
+            x = x/10;
         }
-        
-        cout << pow(2,31);
-        while(x>0){
-            int rem = x%10;
-            ans = ans*10 + rem;
-            x/=10;
-        }
-        if( ans > pow(2,31)){
-            return 0;
-        }
-        if(flag == 1){
-            ans *= -1;
-        }
-        return ans;
+
+        if(r>INT_MAX || r<INT_MIN) return 0;
+        return int(r);
     }
 };
