ef7e4a59e07621ed6093312b6649a0991daf3ba5
Hitesh Wadile <93307471+hitesh-wadile@users.noreply.github.com>
2023-11-24 09:04:37 +0530

    Time: 3 ms (93.37%) | Memory: 6.4 MB (39.48%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+        long long int temp=x,pal=0;
+        if(x<0)
             return false;
-        }
-
-        int c = x;
-        int d;
-        int pol = 0;
-        while (c > 0) {
-            d = c % 10;
-            c = (int)c / 10;
-            if ((pol + d) > INT_MAX/10) {
-                return false;
+        else{
+            while(temp>0){
+                pal = pal*10 + temp%10;
+                temp = temp/10;
             }
-            pol = pol*10 + d;
-            //cout << c << " " << d << endl;
         }
-        return (x == pol);
+        if(x==pal)
+            return true;
+        return false;
     }
 };
