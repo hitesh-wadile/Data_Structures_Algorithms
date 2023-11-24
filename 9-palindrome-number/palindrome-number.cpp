ed6a0d8521d2c46e91c14e3595e47a5753478637
Hitesh Wadile <93307471+hitesh-wadile@users.noreply.github.com>
2023-11-24 09:24:05 +0530

    Time: 19 ms (9.09%) | Memory: 6.2 MB (74.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long int temp=x,pal=0;
-        if(x<0)
-            return false;
-        else{
-            while(temp>0){
-                pal = pal*10 + temp%10;
-                temp = temp/10;
-            }
+        if(x<0 || (x!=0 && x%10==0)) return false;
+        int reversed = 0;
+
+        while (x>reversed){
+            reversed = reversed * 10 + x%10;
+            x = x/10;
         }
-        if(x==pal)
-            return true;
-        return false;
+
+        return (x==reversed) || (x == reversed/10);
     }
 };
